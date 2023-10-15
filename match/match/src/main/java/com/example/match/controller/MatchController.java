41cbc654e1d4679025f5b3b22ff96ae7e14b00c2
olivier <93136015+oliveur83@users.noreply.github.com>
2023-10-15 14:25:59 +0200

    verification et changement des requete

--- match/match/src/main/java/com/example/match/controller/MatchController.java
+++ match/match/src/main/java/com/example/match/controller/MatchController.java
@@ -2,51 +2,67 @@
 package com.example.match.controller;
 import com.example.match.model.Match;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/matches")
 public class MatchController {
 
-    @Autowired
-    private static final Map<Integer, Match> matchService = new HashMap<>();
-
-    @GetMapping("/{id}")
-    public Match getMatchById(@PathVariable int id) {
-        // Implémentez la logique pour récupérer un match par son identifiant
-        Match match = matchService.get(id);
-        return match;
+ @Autowired
+private static final Map<Integer, Match> matchService = new HashMap<Integer, Match>() {
+    {
+        put(1, new Match(1, "toto", "tata"));
+        put(2, new Match(2, "tata", "titi"));
     }
+};
 
-    @PostMapping("/{id}-{id2}")
-    public Match addMatch(@RequestBody Match match) {
-        int id = 0;// Générez un identifiant unique pour le match
-        match.setId(id);
-        matchService.put(id, match);
-        return match;
+@GetMapping("/{id}")
+public ResponseEntity<Match> getMatchById(@PathVariable int id) {
+    // Implémentez la logique pour récupérer un match par son identifiant
+    Match match = matchService.get(id);
+    if (match != null) {
+        return ResponseEntity.ok(match);
+    } else {
+        return ResponseEntity.notFound().build(); // Réponse HTTP 404 si le match n'existe pas
     }
+}
 
-    @PutMapping("/{id}")
-    public Match updateMatch(@PathVariable int id, @RequestBody Match match) {
-        // Implémentez la logique pour mettre à jour un match existant
-        if (matchService.containsKey(id)) {
-            match.setId(id);
-            matchService.put(id, match);
-            return match;
-        } else {
-            return null; // Ou une autre réponse d'erreur
-        }
+@PostMapping("/{name1}-{name2}")
+public ResponseEntity<List<Match>> addMatch(@PathVariable String name1, @PathVariable String name2) {
+    int id1 = matchService.size() + 1; // Générez un identifiant unique pour la première équipe
+    Match match = new Match(id1, name1, name2); // Créez un nouveau match
+ //todo communication 
+    matchService.put(id1, match);
+
+    return ResponseEntity.ok(new ArrayList<>(matchService.values())); // Renvoyez la liste mise à jour des matchs
+}
+
+
+
+@PutMapping("/{id}")
+public ResponseEntity<List<Match>> updateMatch(@PathVariable int id, @RequestBody Match updatedMatch) {
+    // Implémentez la logique pour mettre à jour un match existant
+    if (matchService.containsKey(id)) {
+        updatedMatch.setId(id);
+        matchService.put(id, updatedMatch);
+        return ResponseEntity.ok(new ArrayList<>(matchService.values())); // Renvoyez la liste mise à jour des matchs
+    } else {
+        return ResponseEntity.notFound().build(); // Réponse HTTP 404 si le match n'existe pas
     }
+}
 
-    @DeleteMapping("/{id}")
-    public void deleteMatch(@PathVariable int id) {
-        // Implémentez la logique pour supprimer un match par son identifiant
-        matchService.remove(id);
-    }
-
+@DeleteMapping("/{id}")
+public ResponseEntity<List<Match>> deleteMatch(@PathVariable int id) {
+    // Implémentez la logique pour supprimer un match par son identifiant
+    matchService.remove(id);
+    return ResponseEntity.ok(new ArrayList<>(matchService.values())); // Renvoyez la liste mise à jour des matchs
+}
 
 }
 
